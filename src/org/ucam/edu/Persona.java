3908d6aed3af28e513e9d91e18ce29d9b5008b8c
carcis-bit <124671396+carcis-bit@users.noreply.github.com>
2023-02-16 12:42:19 +0100

    Initial commit

---
+++ src/org/ucam/edu/Persona.java
@@ -0,0 +1,25 @@
+package org.ucam.edu;
+
+public class Persona {
+
+	private int edad;
+
+	public Persona(int edad) {
+		this.edad = edad;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [edad=" + edad + "]";
+	}
+
+	
+}
